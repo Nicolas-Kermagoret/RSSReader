2c8c7865e94da2ab11de9279980be14da69f68b7
Nicolas-Kermagoret <n2kermag@enib.fr>
2016-12-08 21:32:33 +0100

    add pull to refresh

--- app/src/main/java/com/example/nicolas/rssreader/MainActivity.java
+++ app/src/main/java/com/example/nicolas/rssreader/MainActivity.java
@@ -1,38 +1,20 @@
 package com.example.nicolas.rssreader;
 
 import android.content.Context;
-import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Serializable;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
     GetFeedTask asyncTask;
+    boolean isConnected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,28 +25,44 @@ public class MainActivity extends AppCompatActivity {
                 (ConnectivityManager)this.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        boolean isConnected = activeNetwork != null &&
+        this.isConnected = activeNetwork != null &&
                 activeNetwork.isConnectedOrConnecting();
 
-        if(!isConnected){
-//            TextView textConnection = (TextView) this.findViewById(R.id.no_internet);
-//            textConnection.setVisibility(View.VISIBLE);
-//            textConnection.setText("No Internet Connection :(");
+        this.getFeed();
+
+        final SwipeRefreshLayout swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getFeed();
+                swipeContainer.setRefreshing(false);
+            }
+        });
+
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
+    }
+
+    public void getFeed(){
+        if(!this.isConnected){
             LoadFeedFromStorageTask loasTask = new LoadFeedFromStorageTask(this);
             loasTask.execute();
         }
         else{
             try {
-                URL url = new URL("http://www.lemonde.fr/rss/une.xml"); //RSS fedd URL
+                URL url = new URL("http://www.lemonde.fr/rss/une.xml"); //RSS feed URL
                 this.asyncTask = new GetFeedTask(this);
                 this.asyncTask.execute(url); //get articles from rss feed outside the UI Thread
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
         }
-
-
-
     }
 
 }
